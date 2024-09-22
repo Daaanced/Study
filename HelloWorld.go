267f95f55959e1f6fa04c8f17d2804ad1d6ec600
Daaanced <denis_chernushkin@rambler.ru>
2024-09-22 21:45:20 +0700

    switches

--- HelloWorld.go
+++ HelloWorld.go
@@ -2,22 +2,40 @@ package main
 
 import (
 	"fmt"
-
-	"myproject/contacts"
+	"time"
 )
 
-const (
-	one = (iota * 2) + 1
-	three
-	five
-	seven
-	nine
-	eleven
-)
+// var j, i = 10, "hello"
+// const height uint8 = iota + 1
+
+// const VER string = "v1.1.1"
+
+// const (
+// 	one = (iota * 2) + 1
+// 	three
+// 	five
+// 	seven
+// 	nine
+// 	eleven
+// )
 
 func main() {
-	fmt.Println(one, three, five, seven, nine, eleven)
-	contacts.SetSupport("Служба поддержки")
-	//fmt.Println(contacts.GetContact())
-	fmt.Println("Email:", contacts.Email)
+	var a int
+	fmt.Scanf("%d", &a)
+	date := time.Date(2000, 12, 1, 0, 0, 0, 0, time.UTC)
+	switch {
+	case (a <= 1964 && a >= 1946):
+		fmt.Println("Привет, бумер!")
+	case (a <= 1980 && a >= 1965):
+		fmt.Println("Привет, представитель X!")
+	case (a <= 1996 && a >= 1981):
+		fmt.Println("Привет, миллениал!")
+	case (a <= 2012 && a >= 1997):
+		fmt.Println("Привет, зумер!")
+	case (a >= 2013):
+		fmt.Println("Привет, альфа!")
+	default:
+		fmt.Printf("ШО ТО ТЫ ЗАПИДЕЛСЯ СО СВОИМИ %d ГОДКАМИ\n", a)
+	}
+	fmt.Println(date)
 }
